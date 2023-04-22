6960c9168d6ccdff8bca0a18b95bd29770a23706
Antoni Bobiński <bluebirdskyhigh@gmail.com>
2023-04-22 23:32:30 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient"></div>
+      </div>
+    </main>
+  );
+};
+
+export default App;
