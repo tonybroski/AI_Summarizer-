1a91a922e4c8d8e1f96491c67c25a1f06f5e21d3
Antoni Bobiński <bluebirdskyhigh@gmail.com>
2023-04-25 10:15:02 +0200

    display result functionality and local storage

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ const App = () => {
       <div className="main">
         <div className="gradient"></div>
       </div>
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
     </main>
   );
 };
--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,136 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLazyGetSummaryQuery } from "../services/article";
+
+import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({ url: "", summary: "" });
+
+  const [allArticles, setAllArticles] = useState([]);
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+  const [copied, setCopied] = useState("");
+
+  //Load data from localStorage on mount
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const existingArticle = allArticles.find(
+      (item) => item.url === article.url
+    );
+
+    if (existingArticle) return setAllArticles(existingArticle);
+
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      //update state and local storage
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setitem("articles", JSON.stringify(updatedAllArticles));
+    }
+  };
+  //copy the url and toggle the icon for user feedback
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl ">
+      {/* {search} */}
+
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center "
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Enter a URl"
+            value={article.url}
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
+            required
+            className="url_input peer"
+          />
+          <button
+            className="submit_btn peer-focus:border-gray-700 peer-focus:test-gray-700"
+            type="submit "
+          >
+            ↵
+          </button>
+        </form>
+        {/* Browse History*/}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.reverse().map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                <img
+                  src={copied === item.url ? tick : copy}
+                  alt={copied === item.url ? "tick_icon" : "copy_icon"}
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* Display Result*/}
+      <div className="my-10 max-w-full flex justify-center items-center">
+        {isFetching ? (
+          <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+        ) : error ? (
+          <p className="font-inter font-bold text-black">
+            That was not supposed to happen...
+            <br />
+            <span className="font-satoshi font-normal ">
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box ">
+                <p className="font-inter font-medium text-sm text-gray-700"></p>
+                {article.summary}
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Demo;
-// Demo.jsx to show the summary part of the article
